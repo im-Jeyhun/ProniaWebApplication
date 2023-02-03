ec348876b1b860fd829134fd01e3f5a4fc2a686b
Ceyhun <contact.ceyhun@gmail.com>
2023-02-03 15:17:15 +0400

    Slider management for admin panel is completed.

---
+++ DemoApp/DemoApplication/Areas/Admin/ViewModels/Slider/ListItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApplication.Areas.Admin.ViewModels.Slider
+{
+    public class ListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Offer { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string BackgroundImageUrl { get; set; }
+    
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Admin/ViewModels/Slider/UpdateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DemoApplication.Attriubute;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoApplication.Areas.Admin.ViewModels.Slider
+{
+    public class UpdateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Offer { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string BackgroundImageUrl { get; set; }
+        public IFormFile BgImage { get; set; }
+
+        [Required]
+        public string ButtonName { get; set; }
+
+        [Required]
+        public string BtnRedirectUrl { get; set; }
+
+        [Required]
+        public int Order { get; set; }
+    }
+}
