f7bbf66a295766b5d801e76e662bd291011a7616
Ceyhun <contact.ceyhun@gmail.com>
2023-02-03 17:08:03 +0400

    Contact info functionality are completed for admin panel

---
+++ DemoApp/DemoApplication/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using DemoApplication.Areas.Admin.ViewModels.Contact;
+using DemoApplication.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApplication.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/contact")]
+    public class ContactController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ContactController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet("list",Name ="admin-contact-list")]
+        public async Task<IActionResult> List()
+        {
+            var model = _dataContext.Contacts
+                .Select(c => new ListItemViewModel(c.Id, c.Name, c.LastName, c.Phone, c.Email,c.Message)).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Admin/ViewModels/Contact/ListItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoApplication.Areas.Admin.ViewModels.Contact
+{
+    public class ListItemViewModel
+    {
+        public ListItemViewModel(int id, string name, string lastName, string email, string phone, string message)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            Message = message;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+
+    }
+}
