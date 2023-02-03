c4dbcabac6c35dd6ac738fe29e34826227fab1ee
Ceyhun <contact.ceyhun@gmail.com>
2023-02-03 19:30:32 +0400

    Blog area views and view models are added

---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/Blog/BlogItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoApplication.Areas.Client.ViewModels.Blog
+{
+    public class BlogItemViewModel
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string  Title { get; set; }
+        public string Content { get; set; }
+        public string VideoUrl { get; set; }
+        public string ImageUrl { get; set; }
+
+        public List<TagViewModel> Tags { get; set; }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/Blog/BlogListItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApplication.Areas.Client.ViewModels.Blog
+{
+    public class BlogListItemViewModel
+    {
+        public List<CategoryViewModel> Categories { get; set; }
+        public List<TagViewModel> Tags { get; set; }
+
+        public int CategoryId { get; set; }
+        public int TagId { get; set; }
+        public string Search { get; set; }
+    }
+}
