341d548db7772aab5bb84c7afed9be403ff497f7
Ceyhun <contact.ceyhun@gmail.com>
2023-02-03 19:07:35 +0400

    Basket functionality are completed

---
+++ DemoApp/DemoApplication/Areas/Client/Controllers/BasketController.cs
@@ -0,0 +1,106 @@
+﻿using DemoApplication.Areas.Client.ViewComponents;
+using DemoApplication.Areas.Client.ViewModels.Basket;
+using DemoApplication.Areas.Client.ViewModels.Plant;
+using DemoApplication.Database;
+using DemoApplication.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace DemoApplication.Areas.Client.Controllers
+{
+    [Area("client")]
+    [Route("basket")]
+    public class BasketController : Controller
+    {
+        private readonly DataContext _dataContext;
+        private readonly IBasketService _basketService;
+        private readonly IUserService _userService;
+
+        public BasketController(DataContext dataContext, IBasketService basketService, IUserService userService)
+        {
+            _dataContext = dataContext;
+            _basketService = basketService;
+            _userService = userService;
+        }
+
+
+        [HttpPost("add/{id}", Name = "client-basket-add")]
+        public async Task<IActionResult> AddProductAsync([FromRoute] int id , PlantModalListItemViewModal model)
+        {
+            var product = await _dataContext.Plants.FirstOrDefaultAsync(p => p.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            var productsCookieViewModel = await _basketService.AddBasketProductAsync(product , model);
+            if (productsCookieViewModel.Any())
+            {
+                return ViewComponent(nameof(ShopCart), productsCookieViewModel);
+            }
+
+            return ViewComponent(nameof(ShopCart));
+        }
+
+        //[HttpPost("add/{id}", Name = "client-basket-add-bymodal")]
+        //public async Task<IActionResult> AddProductByModalAsync([FromRoute] int id , PlantModalListItemViewModal model)
+        //{
+        //    var product = await _dataContext.Plants
+        //        .Include(p => p.PlantSizes).Include(p => p.PlantColors).FirstOrDefaultAsync(p => p.Id == id);
+
+        //    if (product is null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    //databazada olan plantin olculerini getirmek
+        //    var sizesInDb = product!.PlantSizes.Select(p => p.SizeId).ToList();
+
+        //    //databazada planta aid olan olculeri ayirmaq modeldeki olculerle
+        //    var sizesToRemove = sizesInDb.Except(model.SizeId);
+
+        //    //modelden gelen yeni olculer databasada planta aid olan olculerden ayirmaq
+        //    var sizesToAdd = model.SizeIds.Except(sizesInDb).ToList();
+
+        //    var productsCookieViewModel = await _basketService.AddBasketProductAsync(product);
+        //    if (productsCookieViewModel.Any())
+        //    {
+        //        return ViewComponent(nameof(ShopCart), productsCookieViewModel);
+        //    }
+
+        //    return ViewComponent(nameof(ShopCart));
+        //}
+
+        [HttpGet("delete/{plantId}", Name = "client-basket-delete")]
+        public async Task<IActionResult> DeleteProductAsync([FromRoute] int plantId)
+        {
+            var product = await _dataContext.Plants.FirstOrDefaultAsync(b => b.Id == plantId);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            if (_userService.IsAuthenticated)
+            {
+                var basketProduct = await _dataContext.BasketProducts.FirstOrDefaultAsync(p => p.PlantId == product.Id);
+
+                _dataContext.BasketProducts.Remove(basketProduct);
+                await _dataContext.SaveChangesAsync();
+                return ViewComponent(nameof(ShopCart));
+            }
+            var productCookieValue = HttpContext.Request.Cookies["products"];
+            if (productCookieValue is null)
+            {
+                return NotFound();
+            }
+
+            var productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue);
+            productsCookieViewModel!.RemoveAll(pcvm => pcvm.Id == product.Id);
+
+            HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+
+            return ViewComponent(nameof(ShopCart), productsCookieViewModel);
+        }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/Basket/ProductCookieViewModel.cs
@@ -0,0 +1,83 @@
+﻿namespace DemoApplication.Areas.Client.ViewModels.Basket
+{
+    public class ProductCookieViewModel
+    {
+        public ProductCookieViewModel(int id, string? name, string? imageUrl, int? quantity, decimal? price, decimal? total)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+            Quantity = quantity;
+            Price = price;
+            Total = total;
+        }
+
+        public ProductCookieViewModel(int id, string? name, string? imageUrl, int? quantity, decimal? price, decimal? total, int? colorId, int? sizeId, List<SizeItemViewModel> sizeItemViewModels)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+            Quantity = quantity;
+            Price = price;
+            Total = total;
+            ColorId = colorId;
+            SizeId = sizeId;
+        }
+
+        public ProductCookieViewModel()
+        {
+
+        }
+
+        public ProductCookieViewModel(int id, string? name, string? imageUrl, int? quantity, decimal? price, decimal? total, int? colorId, int? sizeId, List<ColorItemViewModel> colors, List<SizeItemViewModel> sizes)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+            Quantity = quantity;
+            Price = price;
+            Total = total;
+            ColorId = colorId;
+            SizeId = sizeId;
+            Colors = colors;
+            Sizes = sizes;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Total { get; set; }
+        public int? ColorId { get; set; }
+
+        public int? SizeId { get; set; }
+
+        public List<ColorItemViewModel>? Colors { get; set; }
+        public List<SizeItemViewModel>? Sizes { get; set; }
+
+        public class ColorItemViewModel
+        {
+            public ColorItemViewModel(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class SizeItemViewModel
+        {
+            public SizeItemViewModel(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
