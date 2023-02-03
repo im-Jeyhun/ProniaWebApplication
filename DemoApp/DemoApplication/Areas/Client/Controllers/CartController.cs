0d1e70229f1995635927154d9a2d71475bcae86c
Ceyhun <contact.ceyhun@gmail.com>
2023-02-03 19:21:09 +0400

    Cart page are are completed

---
+++ DemoApp/DemoApplication/Areas/Client/Controllers/CartController.cs
@@ -0,0 +1,171 @@
+﻿using DemoApplication.Areas.Client.ViewComponents;
+using DemoApplication.Areas.Client.ViewModels.Basket;
+using DemoApplication.Areas.Client.ViewModels.Plant;
+using DemoApplication.Contracts.File;
+using DemoApplication.Database;
+using DemoApplication.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace DemoApplication.Areas.Client.Controllers
+{
+    [Area("client")]
+    [Route("cart")]
+    public class CartController : Controller
+    {
+        private readonly DataContext _dataContext;
+        private readonly IUserService _userService;
+        private readonly IFileService _fileService;
+        private readonly IBasketService _basketService;
+
+        public CartController(DataContext dataContext, IUserService userService, IFileService fileService, IBasketService basketService)
+        {
+            _dataContext = dataContext;
+            _userService = userService;
+            _fileService = fileService;
+            _basketService = basketService;
+        }
+        [HttpGet("cart-list", Name = "client-cart-list")]
+        public async Task<IActionResult> List()
+        {
+
+            return View();
+        }
+
+        [HttpGet("cart-delete/{plantId}", Name = "client-cart-delete")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int plantId)
+        {
+            if (!_userService.IsAuthenticated)
+            {
+
+                var product = await _dataContext.Plants.FirstOrDefaultAsync(p => p.Id == plantId);
+                if (product is null)
+                {
+                    return NotFound();
+                }
+
+                var productCookieValue = HttpContext.Request.Cookies["products"];
+                if (productCookieValue is null)
+                {
+                    return NotFound();
+                }
+
+                var productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue);
+                productsCookieViewModel!.RemoveAll(pcvm => pcvm.Id == plantId);
+
+                HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+                return ViewComponent(nameof(CartPage), productsCookieViewModel);
+            }
+            else
+            {
+
+                var basketProduct = await _dataContext.BasketProducts
+                        .FirstOrDefaultAsync(bp => bp.Basket.UserId == _userService.CurrentUser.Id && bp.PlantId == plantId);
+
+                if (basketProduct is null) return NotFound();
+
+                _dataContext.BasketProducts.Remove(basketProduct);
+            }
+
+            await _dataContext.SaveChangesAsync();
+            return ViewComponent(nameof(CartPage));
+
+        }
+
+        [HttpGet("cart-invidial-delete/{plantId}", Name = "client-cart-delete-invidual")]
+        public async Task<IActionResult> DeleteInviduallAsync([FromRoute] int plantId)
+        {
+            if (!_userService.IsAuthenticated)
+            {
+
+                var product = await _dataContext.Plants.FirstOrDefaultAsync(p => p.Id == plantId);
+                if (product is null)
+                {
+                    return NotFound();
+                }
+
+                var productCookieValue = HttpContext.Request.Cookies["products"];
+                if (productCookieValue is null)
+                {
+                    return NotFound();
+                }
+
+                var productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue);
+                foreach (var productInCokie in productsCookieViewModel)
+                {
+                    if (productInCokie.Id == product.Id)
+                    {
+
+                        if (productInCokie.Quantity > 1)
+                        {
+                            productInCokie.Quantity -= 1;
+                            productInCokie.Total = productInCokie.Quantity * productInCokie.Price;
+                        }
+                        else
+                        {
+                            productsCookieViewModel.Remove(productInCokie);
+                            break;
+                        }
+                    }
+
+                }
+
+
+                HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+                return ViewComponent(nameof(CartPage), productsCookieViewModel);
+            }
+            else
+            {
+
+                var basketProduct = await _dataContext.BasketProducts
+                        .FirstOrDefaultAsync(bp => bp.Basket.UserId == _userService.CurrentUser.Id && bp.PlantId == plantId);
+
+                if (basketProduct is null) return NotFound();
+
+                if (basketProduct.Quantity >= 1)
+                {
+
+                    basketProduct.Quantity -= 1;
+
+                }
+                else
+                {
+
+                    _dataContext.BasketProducts.Remove(basketProduct);
+
+                }
+
+            }
+
+            await _dataContext.SaveChangesAsync();
+            return ViewComponent(nameof(CartPage));
+
+        }
+
+        [HttpGet("cart-list-async/{id}", Name = "client-cart-list-async")]
+        public async Task<IActionResult> ListAsync(int id , PlantModalListItemViewModal model)
+        {
+            var product = await _dataContext.Plants.FirstOrDefaultAsync(p => p.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            var productsCookieViewModel = await _basketService.AddBasketProductAsync(product , model);
+            if (productsCookieViewModel.Any())
+            {
+                return ViewComponent(nameof(CartPage), productsCookieViewModel);
+            }
+
+            return ViewComponent(nameof(CartPage));
+
+        }
+        [HttpGet("cart-shop-update", Name = "client-shop-update")]
+        public async Task<IActionResult> UpdateShop()
+        {
+            return ViewComponent(nameof(ShopCart));
+
+        }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Client/ViewComponents/CartPage.cs
@@ -0,0 +1,76 @@
+﻿using DemoApplication.Areas.Client.ViewModels.Basket;
+using DemoApplication.Contracts.File;
+using DemoApplication.Database;
+using DemoApplication.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace DemoApplication.Areas.Client.ViewComponents
+{
+    [ViewComponent(Name = "CartPage")]
+    public class CartPage : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        private readonly IUserService _userService;
+        private readonly IFileService _fileService;
+
+        public CartPage(DataContext dataContext, IUserService userService, IFileService fileService)
+        {
+            _dataContext = dataContext;
+            _userService = userService;
+            _fileService = fileService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(List<ProductCookieViewModel> viewModel)
+        {
+
+            // Case 1 : Qeydiyyat kecilib, o zaman bazadan gotur
+            if (_userService.IsAuthenticated)
+            {
+                var model = await _dataContext.BasketProducts
+                    .Where(bp => bp.Basket.UserId == _userService.CurrentUser.Id).
+                    Include(bp => bp.Plant).Include(bp => bp.Plant.PlantColors).Include(bp => bp.Plant.PlantSizes)
+                    .Select(bp =>
+                        new ProductCookieViewModel(
+                            bp.PlantId,
+                            bp.Plant!.Name,
+                            bp!.Plant!.Images.Take(1).FirstOrDefault()! != null ? _fileService.GetFileUrl(bp.Plant.Images!.Take(1)!.FirstOrDefault()!.ImageNameInFileSystem!, UploadDirectory.Plant) : String.Empty,
+                            bp.Quantity,
+                            bp.Plant.Price,
+                            bp.Plant.Price * bp.Quantity , bp.ColorId , bp.SizeId ,
+                              _dataContext.PlantColors.Include(ps => ps.Color).Where(ps => ps.PlantId == bp.PlantId)
+                            .Select(ps => new ProductCookieViewModel.ColorItemViewModel(ps.ColorId, ps.Color.Name)).ToList(),
+                            
+                            _dataContext.PlantSizes.Include(ps => ps.Size).Where(ps => ps.PlantId == bp.PlantId)
+                            .Select(ps => new ProductCookieViewModel.SizeItemViewModel(ps.SizeId , ps.Size.Name)).ToList()
+                          ))
+                    .ToListAsync();
+
+                
+
+                ViewBag.AuthenticatedBasket = model.Count();
+
+                return View(model);
+            }
+
+            if(viewModel is not null)
+            {
+                return View(viewModel);
+            }
+
+
+            //Case 2: Qeydiyyatdan kecmeyib cookiden gotur oxu
+            var productsCookieValue = HttpContext.Request.Cookies["products"];
+            var productsCookieViewModel = new List<ProductCookieViewModel>();
+            if (productsCookieValue is not null)
+            {
+                productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productsCookieValue);
+            }
+
+
+
+            return View(productsCookieViewModel);
+        }
+    }
+}
